d2aea6482dab2235564cea948ee9339b0662e26a
white <whiteboolean@gmail.com>
2025-05-24 09:22:50 +0800

    新增头像备用地址

--- lib/AppImages.dart
+++ lib/AppImages.dart
@@ -1,6 +1,6 @@
 
 class AppImages {
   // 静态常量图片 URL
-  static const String profilePicture =
-      'https://getbubududu.com/wp-content/uploads/2024/12/Who-Created-Bubu-Dudu.png';
+  static const String profilePicture = 'https://getbubududu.com/wp-content/uploads/2024/12/Who-Created-Bubu-Dudu.png';
+  static const String profilePicture1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQXGM_VXvenkbCyEp7bVhfC0DlixkwxhJpRQ&s';
 }
